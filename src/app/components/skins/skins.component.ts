f35ecf65074f7bab391695a2b8f509f60f3cbf23
AlvaroBuenoSosa <Alvarobuso4d@gmail.com>
2024-10-25 11:16:38 +0200

    commit de skins inicial

--- src/app/components/skins/skins.component.ts
+++ src/app/components/skins/skins.component.ts
@@ -7,24 +7,100 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './skins.component.html',
-  styleUrls: ['./skins.component.scss'] // Corrección del nombre de la propiedad
+  styleUrls: ['./skins.component.scss']
 })
 export class SkinsComponent implements OnInit {
-  
-  skin: any;  // Aquí almacenaremos los datos del precio
+
+  skins: any[] = [];  // Inicializado como un array vacío para almacenar varias skins
   error: string | null = null;  // Para manejar errores
 
   constructor(private skinsService: SkinsService) {}
 
   ngOnInit(): void {
-    this.skinsService.getSkinPriceRedline().subscribe({
+    // Obtener el precio de la skin Desert Eagle | Golden Koi
+    this.skinsService.getSkinGoldenKoi().subscribe({
       next: (data) => {
-        this.skin = data;
-        this.error = null; // Resetear el error si la solicitud es exitosa
+        if (data.lowest_price) {  // Verifica que la respuesta contenga datos válidos
+          this.skins.push({ // Agregar la skin al array `skins`
+            name: data.name,  // Nombre de la skin
+            lowest_price: data.lowest_price || 'Precio no disponible',
+            median_price: data.median_price || 'Precio no disponible',
+            image: data.image  // Imagen de la skin
+          });
+          this.error = null; // Resetear el error si la solicitud es exitosa
+        } else {
+          this.error = 'No se pudo obtener el precio de la skin Golden Koi.'; // Manejo de error si no se obtienen datos
+        }
       },
       error: (err) => {
-        this.error = 'Error al obtener los datos: ' + err.message; // Manejo de error
+        this.error = 'Error al obtener los datos: ' + err.message; // Manejo de error de solicitud
       }
     });
+
+    // Obtener el precio de la skin M4A1-S | Hot Rod
+    this.skinsService.getSkinHotRod().subscribe({
+      next: (data) => {
+        if (data.lowest_price) {  // Verifica que la respuesta contenga datos válidos
+          this.skins.push({  // Agregar la skin al array `skins`
+            name: data.name,  // Nombre de la skin
+            lowest_price: data.lowest_price || 'Precio no disponible',
+            median_price: data.median_price || 'Precio no disponible',
+            image: data.image  // Imagen de la skin
+          });
+          this.error = null; // Resetear el error si la solicitud es exitosa
+        } else {
+          this.error = 'No se pudo obtener el precio de la skin Hot Rod.'; // Manejo de error si no se obtienen datos
+        }
+      },
+      error: (err) => {
+        this.error = 'Error al obtener los datos: ' + err.message; // Manejo de error de solicitud
+      }
+    });
+
+    // Obtener el precio de la skin M4A1-S | Player Two
+    this.skinsService.getSkinPlayerTwo().subscribe({
+      next: (data) => {
+        if (data.lowest_price) {  // Verifica que la respuesta contenga datos válidos
+          this.skins.push({  // Agregar la skin al array `skins`
+            name: data.name,  // Nombre de la skin
+            lowest_price: data.lowest_price || 'Precio no disponible',
+            median_price: data.median_price || 'Precio no disponible',
+            image: data.image  // Imagen de la skin
+          });
+          this.error = null; // Resetear el error si la solicitud es exitosa
+        } else {
+          this.error = 'No se pudo obtener el precio de la skin Hot Rod.'; // Manejo de error si no se obtienen datos
+        }
+      },
+      error: (err) => {
+        this.error = 'Error al obtener los datos: ' + err.message; // Manejo de error de solicitud
+      }
+    });
+
+        // Obtener el precio de la skin M4A1-S | PrintStream
+        this.skinsService.getSkinPrintStream().subscribe({
+          next: (data) => {
+            if (data.lowest_price) {  // Verifica que la respuesta contenga datos válidos
+              this.skins.push({  // Agregar la skin al array `skins`
+                name: data.name,  // Nombre de la skin
+                lowest_price: data.lowest_price || 'Precio no disponible',
+                median_price: data.median_price || 'Precio no disponible',
+                image: data.image  // Imagen de la skin
+              });
+              this.error = null; // Resetear el error si la solicitud es exitosa
+            } else {
+              this.error = 'No se pudo obtener el precio de la skin Hot Rod.'; // Manejo de error si no se obtienen datos
+            }
+          },
+          error: (err) => {
+            this.error = 'Error al obtener los datos: ' + err.message; // Manejo de error de solicitud
+          }
+        });
+
   }
+
+  
 }
+
+
+
--- src/app/shared/services/skins.service.ts
+++ src/app/shared/services/skins.service.ts
@@ -1,21 +1,74 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkinsService {
   private apiUrl = '/market/priceoverview/'; // Cambiado a la ruta del proxy
+  private imageBaseUrl = 'https://community.cloudflare.steamstatic.com/economy/image/';
 
   constructor(private http: HttpClient) {}
 
-  getSkinPriceRedline(): Observable<any> {
+  getSkinGoldenKoi(): Observable<any> {
     const params = new HttpParams()
-      .set('appid', '730')
-      .set('currency', '3')
-      .set('market_hash_name', 'AK-47 | Redline (Field-Tested)');
+      .set('appid', '730') // App ID de CS:GO
+      .set('currency', '3') // Código de moneda (3 para Euros)
+      .set('market_hash_name', 'Desert Eagle | Golden Koi (Factory New)'); // Nombre del artículo
 
-    return this.http.get<any>(this.apiUrl, { params });
+    return this.http.get<any>(this.apiUrl, { params }).pipe(
+      map((data: any) => ({
+        ...data,
+        name: 'Desert Eagle | Golden Koi (Factory New)', // Nombre de la skin
+        image: this.imageBaseUrl + '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposr-kLAtl7PLFTi5B7dCzh7-JhfbiPITdn2xZ_Ismi7DA9tWg0VHm_EVtY2iicoTAdAI3YFGBq1XowObp05K9v8-YnycxpGB8ssNrD69j/360fx360f', // URL de la imagen
+      }))
+    );
   }
+
+  getSkinHotRod(): Observable<any> {
+    const params = new HttpParams()
+      .set('appid', '730') // App ID de CS:GO
+      .set('currency', '3') // Código de moneda (3 para Euros)
+      .set('market_hash_name', 'M4A1-S | Hot Rod (Factory New)'); // Nombre del artículo
+
+    return this.http.get<any>(this.apiUrl, { params }).pipe(
+      map((data: any) => ({
+        ...data,
+        name: 'M4A1-S | Hot Rod (Factory New)', // Nombre de la skin
+        image: this.imageBaseUrl + '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uO3mr-ZkvPLPu_Qx3hu5Mx2gv2Pp9yn31Li_ERtYW70dYaXdFI8NVvYq1i7xOrohcTt7sudySZnsyNz7GGdwUICPND1TA/360fx360f', // URL de la imagen
+      }))
+    );
+  }
+
+  getSkinPlayerTwo(): Observable<any> {
+    const params = new HttpParams()
+      .set('appid', '730') // App ID de CS:GO
+      .set('currency', '3') // Código de moneda (3 para Euros)
+      .set('market_hash_name', 'M4A1-S | Player Two (Factory New)'); // Nombre del artículo
+
+    return this.http.get<any>(this.apiUrl, { params }).pipe(
+      map((data: any) => ({
+        ...data,
+        name: 'M4A1-S | Player Two (Factory New)', // Nombre de la skin
+        image: this.imageBaseUrl + '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uO1gb-Gw_alIITShWxeupUl0tbN_Iv9nBqxrUA9MWmhINKUIFU9N1rT_AK3lbjnjcLo6ZuazXA1u3R34yzdzha_n1gSORVcLpnc/360fx360f', // URL de la imagen
+      }))
+    );
+  }
+
+  getSkinPrintStream(): Observable<any> {
+    const params = new HttpParams()
+      .set('appid', '730') // App ID de CS:GO
+      .set('currency', '3') // Código de moneda (3 para Euros)
+      .set('market_hash_name', 'M4A1-S | Printstream (Factory New)'); // Nombre del artículo
+
+    return this.http.get<any>(this.apiUrl, { params }).pipe(
+      map((data: any) => ({
+        ...data,
+        name: 'M4A1-S | Printstream (Factory New)', // Nombre de la skin
+        image: this.imageBaseUrl + '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uO1gb-Gw_alIITBhGJf_NZlmOzA-LP5gVO8v11qa2n6dtOcIQVoMFHUqwC9wei7jcO5vZ3AzSQ1vCMls3fayxKyhh1McKUx0sfzkVMr/360fx360f', // URL de la imagen
+      }))
+    );
+  }
+  
 }
