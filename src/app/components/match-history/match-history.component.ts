14d97034011f44e67eca9c7b1abe0d756407cd35
AlvaroBuenoSosa <Alvarobuso4d@gmail.com>
2024-10-25 11:54:57 +0200

    Commit principio de match-history y profile

--- src/app/components/match-history/match-history.component.ts
+++ src/app/components/match-history/match-history.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatchHistoryService } from '../../shared/services/match-history.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-match-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './match-history.component.html',
   styleUrl: './match-history.component.scss'
 })
-export class MatchHistoryComponent {
+export class MatchHistoryComponent implements OnInit {
 
+  playerProfile: any;
+  ownedGames: any[] = [];
+  csGoStats: any;
+
+  constructor(private matchHistoryService: MatchHistoryService) {}
+
+  ngOnInit(): void {
+    this.loadPlayerProfile();
+    this.loadOwnedGames();
+    this.loadCsGoStats();
+  }
+
+  loadPlayerProfile() {
+    this.matchHistoryService.getPlayerProfile().subscribe({
+      next: (data) => {
+        this.playerProfile = data.response.players[0];
+        console.log(this.playerProfile);
+      },
+      error: (err) => {
+        console.error('Error fetching player profile:', err);
+      }
+    });
+  }
+
+  loadOwnedGames() {
+    this.matchHistoryService.getOwnedGames().subscribe({
+      next: (data) => {
+        this.ownedGames = data.response.games;
+        console.log(this.ownedGames);
+      },
+      error: (err) => {
+        console.error('Error fetching owned games:', err);
+      }
+    });
+  }
+
+  loadCsGoStats() {
+    this.matchHistoryService.getCsGoStats().subscribe({
+      next: (data) => {
+        this.csGoStats = data;
+        console.log(this.csGoStats);
+      },
+      error: (err) => {
+        console.error('Error fetching CS:GO stats:', err);
+      }
+    });
+  }
 }
--- src/app/shared/services/match-history.service.ts
+++ src/app/shared/services/match-history.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchHistoryService {
 
-  constructor() { }
+  private apiKey = '391CE3115B6FEBFA7800F5AD9F614BE5'; // Reemplaza con tu API Key
+  private steamId = '76561198917617584'; // ID de Steam de LGenDOfficial
+
+  private apiUrlProfile = `/api/ISteamUser/GetPlayerSummaries/v0002/?key=${this.apiKey}&steamids=${this.steamId}`;
+  private apiUrlDatosJuegos = `/api/IPlayerService/GetOwnedGames/v0001/?key=${this.apiKey}&steamid=${this.steamId}&include_appinfo=true&include_played_free_games=true`;
+  private apiUrlStatsCs = `/api/ISteamUserStats/GetGlobalStatsForGame/v0001/?appid=730&count=1&name[0]=total_kills&key=${this.apiKey}`;;
+
+  constructor(private http: HttpClient) {}
+
+  getPlayerProfile(): Observable<any> {
+    return this.http.get<any>(this.apiUrlProfile);
+  }
+
+  getOwnedGames(): Observable<any> {
+    return this.http.get<any>(this.apiUrlDatosJuegos);
+  }
+
+  getCsGoStats(): Observable<any> {
+    return this.http.get<any>(this.apiUrlStatsCs);
+  }
 }
